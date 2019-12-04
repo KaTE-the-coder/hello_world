8085664b5b4c935093897094e7ecc7e32fab4786
KaTE-the-coder <58459648+KaTE-the-coder@users.noreply.github.com>
2019-12-04 19:25:03 +0700

    Edits of main.cpp
    Add another line after the "Hello world!" line to say I am new to Google Code-in.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-
+//add another line after "Hello world!" line.
 
 int main()
 {
-    std::cout << "Hello world!";
+    std::cout << "Hello world!"<<'\n';
+    std::cout << "I am new to Google Code-in, please love me!";
     return 0;
 }
